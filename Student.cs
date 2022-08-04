621a43aeec88a43594829dd732a2c5281b322f17
Sameermurtaza12 <107950748+Sameermurtaza12@users.noreply.github.com>
2022-08-04 06:52:32 -0400

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Services;
+using System.Configuration;
+
+namespace Assignment5Task1
+{
+    public class Student
+    {
+        public string student_name { get; set; }
+        public string student_address { get; set; }
+        public string student_contact { get; set; }
+        public int student_age { get; set; }
+        public int student_id { get; set; }
+
+        [WebMethod]
+        public List<Student> GetStudentList()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            List<Student> studenttList = new List<Student>();
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+
+                SqlCommand cmd = new SqlCommand("spGetStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.student_id = Convert.ToInt32(rdr["student_id"]);
+                    student.student_age = Convert.ToInt32(rdr["student_age"]);
+                    student.student_contact = rdr["student_contact"].ToString();
+                    student.student_address = rdr["student_address"].ToString();
+                    student.student_name = rdr["student_name"].ToString();
+                    studenttList.Add(student);
+                }
+
+            }
+            return studenttList;
+
+        }
+      
+    }
+}
---
+++ ValuesController1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+namespace Assignment5Task1
+{
+    public class ValuesController1 : ApiController
+    {
+        // GET api/<controller>
+       
+        // GET api/<controller>/5
+
+    }
+}
